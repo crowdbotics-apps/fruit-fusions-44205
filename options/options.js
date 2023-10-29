bac0e14e237e722c005fd778e4425c0adcc9872a
Crowdbotics <team@crowdbotics.com>
2023-10-29 20:42:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fruit-fusions-44205",
+  url: "https://fruit-fusions-44205.botics.co",
+  api: "https://fruit-fusions-44205.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
